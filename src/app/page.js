dc27e259e0aca258e5f163d636d116e4cbf30689
Anix <baranish2003@gmail.com>
2025-09-20 13:47:41 +0530

    Fixing log in button

--- src/app/page.js
+++ src/app/page.js
@@ -73,10 +73,11 @@ export default function Home() {
               </div>
             </div>
             <div className="flex items-center space-x-4">
-              <span className="inline-flex items-center px-3 py-1 rounded-full text-xs font-medium bg-green-100 text-green-800">
+              {/* <span className="inline-flex items-center px-3 py-1 rounded-full text-xs font-medium bg-green-100 text-green-800">
                 <div className="w-2 h-2 bg-green-400 rounded-full mr-2"></div>
                 System Online
-              </span>
+              </span> */}
+              <button className="flex items-center bg-blue-100 hover:bg-blue-400 hover:text-white py-2 px-4 rounded-full transition-all duration-200 ease-in-out">Log In</button>
             </div>
           </div>
         </div>
